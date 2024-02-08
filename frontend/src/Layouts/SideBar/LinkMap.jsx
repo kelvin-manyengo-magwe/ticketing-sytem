8bbe5c0fb9e9e012d709dab1e3cbd8d71a8de402
kelvinmagwe <kelvinmular5@gmail.com>
2024-02-08 17:05:43 -0500

    Restructured navigation using react-router

---
+++ frontend/src/Layouts/SideBar/LinkMap.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link, Outlet, useNavigate } from 'react-router-dom';
+import './SideBar.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+
+const LinkMap = () => {
+    const menuItems = [
+        {path: '/dashboard',  icon: 'bi bi-grid',  text: 'Dashboard'},
+        {path: '/tickets',  icon: 'bi bi-ticket-detailed',  text: 'Tickets'},
+        {path: '/departments',  icon: 'bi bi-bookshelf',  text: 'Departments'},
+        {path: '/notifications',  icon: 'bi bi-bell',  text: 'Notifications'},
+        {path: '/users',  icon: 'bi bi-people',  text: 'Users'},
+        {path: '/settings',  icon: 'bi bi-gear',  text: 'Settings'},
+        {path: '/reports', icon: 'bi bi-book', text: 'Reports'},
+        {path: '/logout',  icon: 'bi bi-box-arrow-left',  text: 'Logout'},
+    ];
+
+  return (
+      <ul className="listContent">
+          {menuItems.map( (item, index) => (
+              <li key={index}>
+                  <Link to={item.path}>
+                      <i className={item.icon}></i>
+                      <span>{item.text}</span>
+                  </Link>
+              </li>
+          )
+          )}
+      </ul>
+  );
+}
+
+export default LinkMap;
